286313f9a1f90a2e75f5493d85107fdf1d13e961
vinicius maia silveira <vinimaia.prof@gmail.com>
2020-05-14 11:40:21 -0600

    first commit2

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { auth } from '../../firebase/firebase.utils';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div class="userDropdown">{currentUser.displayName}
+          <div class="userDropdownContent">
+            <div><a>Edit User</a></div>
+            <div className='option' onClick={() => auth.signOut()}>
+              SIGN OUT
+        </div>
+          </div>
+        </div>
+      ) : (
+          <Link className='option' to='/signin'>
+            SIGN IN
+          </Link>
+        )}
+    </div>
+  </div>
+);
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
